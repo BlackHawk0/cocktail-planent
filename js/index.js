903ec93f3d61e26874c84f901bd77ca5729af4f0
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-09-05 15:16:28 +0300

    adding comments

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
+document.addEventListener('DOMContentLoaded', ()=> console.log('Content loaded'))
+
+
+
+
 // Fetch the top 10 cocktails from thecocktaildb API
 function getPopularCocktails(){
     fetch('https://www.thecocktaildb.com/api/json/v2/9973533/popular.php')
@@ -13,7 +18,7 @@ function getPopularCocktails(){
         // change the response to json
         response.json().then(data => {
             //pass the data to mapData function   (data in json format)
-            mapData(data)
+            renderPopularCocktails(data)
         });
     }
     )
@@ -36,7 +41,7 @@ function getRecentCocktails(){
         }
 
         ///pass the data to mapData function   (data in json format)
-        response.json().then(data => mapData(data));
+        response.json().then(data => renderRecentCocktails(data));
     }
     )
     // If response is not successfull show error
@@ -72,7 +77,8 @@ function getRandomCocktail(){
 }
 
 
-function mapData(cocktails){
+function renderRecentCocktails(cocktails){
+
     let bestThumbs = cocktails.drinks.map(function(cocktail){
        let cocktailImages = cocktail.strDrinkThumb
        return cocktailImages
@@ -82,18 +88,41 @@ function mapData(cocktails){
         let cocktailList = cocktail.strDrink
         return cocktailList   
     });
-    renderPopularCocktails(bestThumbs, bestCocktails)
-    renderRecentCocktails(bestThumbs, bestCocktails)
-}
-
-
-function renderRecentCocktails(cocktailimages, cocktailnames){
 
     let imageContainer = document.querySelector('#recent')
 
-    for (let i = 0; i < 4; i++){
-        let bestList = cocktailnames[i]
-        let cocktailimage = cocktailimages[i]
+    for (let i = 0; i < 8; i++){
+        let bestList = bestCocktails[i]
+        let cocktailimage = bestThumbs[i]
+        const item = document.createElement('h4')
+        const image = document.createElement('img')
+
+        item.innerText = `${bestList}`
+        image.src = `${cocktailimage}`
+        imageContainer.appendChild(item)
+        imageContainer.appendChild(image)
+    }
+
+}
+
+
+function renderPopularCocktails(cocktails){
+
+    let bestThumbs = cocktails.drinks.map(function(cocktail){
+       let cocktailImages = cocktail.strDrinkThumb
+       return cocktailImages
+        
+    });
+    let  bestCocktails = cocktails.drinks.map(function(cocktail){
+        let cocktailList = cocktail.strDrink
+        return cocktailList   
+    });
+
+    let imageContainer = document.querySelector('#latest')
+
+    for (let i = 0; i < 8; i++){
+        let bestList = bestCocktails[i]
+        let cocktailimage = bestThumbs[i]
         const item = document.createElement('h4')
         const image = document.createElement('img')
 
@@ -103,7 +132,9 @@ function renderRecentCocktails(cocktailimages, cocktailnames){
         imageContainer.appendChild(image)
 
     }
+
 }
+
 
 
 function renderRandomCocktail(cocktail){
@@ -117,26 +148,88 @@ function renderRandomCocktail(cocktail){
 
 }
 
-function renderPopularCocktails(images, names){
 
-    let imageContainer = document.querySelector('#latest')
-
-    for (let i = 0; i < 4; i++){
-        let bestList = names[i]
-        let cocktailimage = images[i]
-        const item = document.createElement('h4')
-        const image = document.createElement('img')
-
-        item.innerText = `${bestList}`
-        image.src = `${cocktailimage}`
-        imageContainer.appendChild(item)
-        imageContainer.appendChild(image)
-
-    }
-}
 
 
 // function calls
 getRecentCocktails()
 getPopularCocktails()
 getRandomCocktail();
+
+
+// event listners
+
+const commentContainer = document.getElementById('allComments');
+document.getElementById('addComments').addEventListener('click', function (ev) {
+   addComment(ev);
+});
+
+function addComment(ev) {
+    let commentText, wrapDiv;
+    const textBox = document.createElement('div');
+    const replyButton = document.createElement('button');
+    replyButton.className = 'reply';
+    replyButton.innerHTML = 'Reply';
+    const likeButton = document.createElement('button');
+    likeButton.innerHTML = 'Like';
+    likeButton.className = 'likeComment';
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = 'Delete';
+    deleteButton.className = 'deleteComment';
+    if(hasClass(ev.target.parentElement, 'container')) {
+        const wrapDiv = document.createElement('div');
+        wrapDiv.className = 'wrapper';
+        wrapDiv.style.marginLeft = 0;
+        commentText = document.getElementById('comment').value;
+        document.getElementById('comment').value = '';
+        textBox.innerHTML = commentText;
+        textBox.style.backgroundColor = "cornflowerblue";
+        wrapDiv.append(textBox, replyButton, likeButton, deleteButton);
+        commentContainer.appendChild(wrapDiv);
+    } else {
+        wrapDiv = ev.target.parentElement;
+        commentText = ev.target.parentElement.firstElementChild.value;
+        textBox.innerHTML = commentText;
+        textBox.style.backgroundColor = "paleturquoise";
+        wrapDiv.innerHTML = '';
+        wrapDiv.append(textBox, replyButton, likeButton, deleteButton);
+    }
+    // setOnLocalStorage();
+}
+
+// function setOnLocalStorage () {
+//     localStorage.setItem('template', document.getElementById('allComments').innerHTML);
+// }
+function hasClass(elem, className) {
+    return elem.className.split(' ').indexOf(className) > -1;
+}
+document.getElementById('allComments').addEventListener('click', function (e) {
+    if (hasClass(e.target, 'reply')) {
+        const parentDiv = e.target.parentElement;
+        const wrapDiv = document.createElement('div');
+        wrapDiv.style.marginLeft = (Number.parseInt(parentDiv.style.marginLeft) + 15).toString() + 'px';
+        wrapDiv.className = 'wrapper';
+        const textArea = document.createElement('textarea');
+        textArea.style.marginRight = '20px';
+        const addButton = document.createElement('button');
+        addButton.className = 'addReply';
+        addButton.innerHTML = 'Add';
+        const cancelButton = document.createElement('button');
+        cancelButton.innerHTML = 'Cancel';
+        cancelButton.className='cancelReply';
+        wrapDiv.append(textArea, addButton, cancelButton);
+        parentDiv.appendChild(wrapDiv);
+
+    } else if(hasClass(e.target, 'addReply')) {
+        addComment(e);
+    } else if(hasClass(e.target, 'likeComment')) {
+         const likeBtnValue = e.target.innerHTML;
+         e.target.innerHTML = likeBtnValue !== 'Like' ? Number.parseInt(likeBtnValue) + 1 : 1;
+        setOnLocalStorage();
+    } else if(hasClass(e.target, 'cancelReply')) {
+        e.target.parentElement.innerHTML = '';
+        setOnLocalStorage();
+    } else if(hasClass(e.target, 'deleteComment')) {
+        e.target.parentElement.remove();
+    }
+});
